1400d6ca1da2280eecfff95418b509ef7afd4951
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-01-15 11:22:22 +0545

    array

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        String[] names ={"java","c",};
+        for(String name:names){
+            System.out.println("name: "+name);
+        }
+    }
+}
